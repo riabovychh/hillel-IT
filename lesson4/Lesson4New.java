5fe566692a569024092f66ac542967d6f1141489
Tetiana Riabovych <t.riabovych@gmail.com>
2025-02-05 12:53:17 +0100

    fixes

--- lesson4/Lesson4New.java
+++ lesson4/Lesson4New.java
@@ -1,5 +1,5 @@
 public class Lesson4New {
     public static void main(String[] args) {
-        System.out.println("2. Об'єм циліндра з радіусом "
+        System.out.println("2. Об'єм циліндра з радіусом ");
     }
 }
