806ab5251655e4b68881882e2a6d2674b4055da2
Tetiana Riabovych <t.riabovych@gmail.com>
2025-02-08 11:33:44 +0100

    updated Lesson4 with methods

--- lesson4/Lesson4.java
+++ lesson4/Lesson4.java
@@ -1,23 +1,38 @@
-import java.sql.SQLOutput;
-
 public class Lesson4 {
+
+    public static int square (int firstNumber) {                     // метод для розрахунку квадрату числа
+        int squareResult = firstNumber * firstNumber;
+        return squareResult;
+    }
+
+    public static double volume (double radius, double height) {       // метод розрахунку об'єму цилiндра
+        double volumeCalculation = Math.PI * Math.pow(radius, 2) * height;
+        return volumeCalculation;
+    }
+
+    public static int calcToPower (int firstNumberA, int secondNumberB) {    // метод розрахунку одного числа в степенi iншого
+        int powerCalculation = (int) Math.pow(firstNumberA, secondNumberB);
+        return powerCalculation;
+    }
+
+
     public static void main(String[] args) {
-        int x = 5;
-        int y = x*x;
-        System.out.println("1. Введіть ціле число: " + x);
-        System.out.println("   Квадрат числа " + x + " дорівнює " + y);
 
-        double pi = 3.14159265358979323846;
-        double radius = 4.53;
-        double high = 11.52;
-        double v =  pi*Math.pow(radius, 2)*high;
-        System.out.println("2. Об'єм циліндра з радіусом " + radius + " і висотою " + high + " дорівнює " + v);
+        int firstNumber = Integer.parseInt(args[0]);
+        int squareResult = square(firstNumber);
+        System.out.println("1. Введіть ціле число: " + firstNumber);
+        System.out.println("   Квадрат числа " + firstNumber + " дорівнює " + squareResult);
 
-        int a = 4;
-        int b = 3;
-        int c = (int) Math.pow(a, b);
-        System.out.println("3. Введіть a: " + a);
-        System.out.println("   Введіть b: " + b);
-        System.out.println("   Результат " + a + "^" + b + " дорівнює " + c);
+        double radius = Double.parseDouble(args[1]);
+        double height = Double.parseDouble(args[2]);
+        double volumeCalcuation = volume(radius, height);
+        System.out.println("2. Об'єм циліндра з радіусом " + radius + " і висотою " + height + " дорівнює " + volumeCalcuation);
+
+        int firstNumberA = Integer.parseInt(args[3]);
+        int secondNumberB = Integer.parseInt(args[4]);
+        int powerCalculation = calcToPower(firstNumberA, secondNumberB);
+        System.out.println("3. Введіть a: " + firstNumberA);
+        System.out.println("   Введіть b: " + secondNumberB);
+        System.out.println("   Результат " + firstNumberA + "^" + secondNumberB + " дорівнює " + powerCalculation);
     }
 }
