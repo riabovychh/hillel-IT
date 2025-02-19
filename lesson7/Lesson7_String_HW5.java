e024113b8d10d5616126733d45d12954dcabd7dc
Tetiana Riabovych <t.riabovych@gmail.com>
2025-02-19 21:10:53 +0100

    HW 7

---
+++ lesson7/Lesson7_String_HW5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Lesson7_String_HW5 {
+    public static boolean xyzThere(String enteredString) {
+
+        for (int i = 0; i <= enteredString.length() - 3; i++) { // перебираю строку с учетом того, что она может заканчиваться на xyz
+            if (enteredString.substring(i, i + 3).equals("xyz")) {  // проверяю наличие xyz
+                if (i > 0 && enteredString.charAt(i - 1) == '.') {  // проверяю, что перед найденным xyz нет точки
+                    return false;  // если точка спереди есть - false
+                }
+                return true;  // если точки спереди нет - true
+            }
+        }
+        return false;  // если xyz не найдено -  false
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter a string ( " + (i + 1) + " try): ");
+            String enteredString = scanner.nextLine();
+            System.out.println("You have entered: " + enteredString);
+            System.out.println("Result: " + xyzThere(enteredString));
+        }
+
+        System.out.println("The cycle is finished.");
+
+        scanner.close();
+    }
+}
